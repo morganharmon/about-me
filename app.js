01bdcfb7f52314fd9f0083181480589b504c09f7
Morgan Harmon <morganharmon@gmail.com>
2022-04-20 15:15:07 -0700

    grab DOM elements and add hidden div around img

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // import functions
 
 // grab DOM elements
+const dog = document.getElementById('dog');
+const button = document.getElementById('button');
 
 // set event listeners 
     // get user input
